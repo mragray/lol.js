773e03e46f40fb5370af66d36126e375dd8f06ba
Aaron Gray <aaronsallright@gmail.com>
2014-07-07 13:51:59 -0600

    Update readme

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
-# lol.js makes it easy to provide inline form validation and save progress in local storage incase of interruption.
+# lol.js: Better forms for much win.
 
-## This is a work in progress and is not ready for use.
+## lol.js makes it easy to provide inline form validation and save progress in local storage incase of interruption. _This is a work in progress and is not ready for anything._
+
+## Values:
+1. Provide visual feedback as users fill out forms.
+2. Save progress locally as users fill out forms.
+3. Reduce HTTP requests by leveraging local storage.
+4. Delegate javascript events for much win.
+
+### As is...
+```javascript
+	$('input').lol();
+```
+
+```javascript
+	$('input[type="tel"]').lol();
+```
+
+### Soon!
+```javascript
+	$(yourSelection).lol({
+		pattern: {regex},		// See http://html5pattern.com/
+		matches: {id}, 			// for confirmation fields
+		invalidClass: {class}, 	// defaults is false
+		validClass: {class} 	// defaults is false
+	});
+```
 
 ### Todo (in no order):
-- Include settings parrams
-- Make lol global
-- use event delegation for form target
-- write docs
-
-### Todo later:
-- use module exports for node / browserify modules
+- Handle invalide form fields.
+- Pass options as an argument to lol(). See "Soon!" above.
+- use module exports for node / browserify modules to validate on client & server
+- Update bower, jquery, and npm package info with version 1.0
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function($, window, document, undefined){
+(function( $, window, document, undefined ){
 
   var Field = {
     init: function( options, elem ) {
@@ -6,42 +6,37 @@
           
       self.elem = elem;
       self.$elem = $( elem );
-      self.id = self.$elem.attr('id');
+      self.id = self.$elem.attr( 'id' );
     },
-    storage: function () {
+    storage: function ( ) {
       var self = this;
 
-      var storedValue = localStorage.getItem(self.id);
-      if (storedValue) {
-        self.$elem.val(storedValue);
+      var storedValue = localStorage.getItem( self.id );
+      if ( storedValue ) {
+        self.$elem.val( storedValue );
       }
     },
     handle: function() {
       var self = this;
       
-      self.$elem.on('blur', this.$elem.parent('form'), function(e) {
-        if (e.target.checkValidity()) {
-          console.log('valid');
-          localStorage.setItem(self.id, event.target.value);
+      self.$elem.on('blur', this.$elem.parent( 'form' ), function( e ) {
+        if ( e.target.checkValidity() ) {
+          console.log( 'valid' );
+          localStorage.setItem( self.id, event.target.value );
         } else {
-          console.log('nope!');
+          console.log( 'nope!' );
         }
       });      
     }
   }
 
   $.fn.lol = function( options ) {
-    return this.each(function(){
-      var field = Object.create(Field);
-      field.init(options, this);
-      field.storage();
-      field.handle();
+    return this.each( function( ){
+      var field = Object.create( Field );
+      field.init( options, this );
+      field.storage( );
+      field.handle( );
     });
-  }
-  
-  $.fn.lol.fieldOptions = {
-    id: null,
-    message: null
   }
 
   $.fn.lol.options = {
@@ -50,4 +45,4 @@
     invalideClass: false
   }
 
-})(jQuery, window, document );
+})( jQuery, window, document );
